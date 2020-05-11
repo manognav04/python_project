7d0314e9758f7e7f3b7649d5ef337f6d281d3135
manognav <manognav@Manognas-MBP.attlocal.net>
2020-05-10 22:22:56 -0500

    input and string convertion

---
+++ inputChallenge.py
@@ -0,0 +1,14 @@
+#1. What is your name?
+
+name = input("What is your name?")
+print("My name is " + name + ".")
+print(type(name))
+
+#2. What is your quest?
+
+question = input("What is your quest?")
+print("my question is " + question + ".")
+
+#3. What is your favorite color?
+color = input("What is your favorite color?")
+print("My fav color is " + color + ".")
---
+++ stringToInteger.py
@@ -0,0 +1,7 @@
+# In a Python file, use input() to ask the user to enter an integer that 10 will be added to.
+# Assign what they type to a variable.
+# print() the sum of the integer they entered and 10.
+
+user_int = int(input("please enter an integer "))
+print(user_int + 10)
+print(type(user_int))
